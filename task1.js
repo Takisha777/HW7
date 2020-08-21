8644a2976f69747fdd0fc12cf5c69f8bf3990651
Takisha <alexey.kovtun.od@gmail.com>
2020-08-21 15:04:36 +0300

    1

--- task1.js
+++ task1.js
@@ -2,3 +2,22 @@
 //     Написать код, который переберёт оба объекта и сконкатениурет все поля-массивы.
 //     Посчитать сумму чисельных элементов в массивах.
 
+const userObj = {
+    name: 'John',
+    age: 7,
+    surname: 'Connor',
+    man: true,
+    greet: greetFunc()
+}
+const userObj2 = {
+    id: 1,
+    name: 'Jany',
+    age: 10,
+    man: false,
+    greet: greetFunc()
+}
+function greetFunc() {
+    return arguments;
+}
+const userObj3 = {...userObj, ...userObj2};
+console.log(userObj3);
