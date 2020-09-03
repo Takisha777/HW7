79ef6ef9640039bc5f1548b50270d89f43126c8a
Takisha <alexey.kovtun.od@gmail.com>
2020-09-03 15:25:35 +0300

    final

--- task3.js
+++ task3.js
@@ -1,3 +1,20 @@
 // 3.   Написать методы push, join, reverse самостоятельно. Их функциональность должна
 //      соответствовать стандартным методам массивов.
 //      list.myPush(); list.myJoin(); list.myReverse();
+
+let arr = [1, 2 ,3 , 'red', NaN];
+Array.prototype.myPush = function (userElement){
+    arr[arr.length] = userElement;
+    return arr.length;
+};
+Array.prototype.myJoin = function (userSym) {
+    return arr.join(userSym);
+};
+Array.prototype.myReverse = function () {
+    arr.reverse();
+    return arr;
+};
+
+document.writeln(arr.myPush(23)+ '<br>');
+document.writeln(arr.myJoin('+') + '<br>');
+document.writeln(arr.myReverse());
